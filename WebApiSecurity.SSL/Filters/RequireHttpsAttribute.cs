7d4717e67f7d1ca5835b5fd4de640ffe6955d1ca
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2017-12-11 21:45:54 +0100

    analiza certyfikatu

--- WebApiSecurity.SSL/Filters/RequireHttpsAttribute.cs
+++ WebApiSecurity.SSL/Filters/RequireHttpsAttribute.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
@@ -15,12 +16,30 @@ namespace WebApiSecurity.SSL.Filters
             if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
             {
                 actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
-                { 
+                {
                     ReasonPhrase = "HTTPS Required"
                 };
             }
             else
             {
+                X509Certificate2 cert = actionContext.Request.GetClientCertificate();
+
+                if (cert == null)
+                {
+                    actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+                    {
+                        ReasonPhrase = "Client Certificate Required"
+                    };
+                }
+
+                else
+                {
+                    string issuer = cert.Issuer;
+                    string subject = cert.Subject;
+
+
+                }
+
                 base.OnAuthorization(actionContext);
             }
         }
